8801d3ab235b2c18496367d6db53181a9cf12c6f
amwebexpert <amwebexpert@gmail.com>
2019-12-14 16:32:07 -0500

    Improved perf test and added deploy script for Pivotal cloud platform development test

--- deploy-pcf.sh
+++ deploy-pcf.sh
@@ -1,3 +1,6 @@
 ./mvnw clean install
 cf push idgen -p target/idgen-0-SNAPSHOT.jar
-cf logs idgen
+# cf logs idgen
+
+echo Starting browser at server welcome page
+google-chrome --user-data-dir=/tmp https://idgen.cfapps.io/
